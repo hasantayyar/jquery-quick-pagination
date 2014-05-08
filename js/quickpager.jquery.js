1066fe23149d4a286373db06051c187243a2d456
hasantayyar <tayyar.besik@gmail.com>
2014-05-08 12:03:14 +0300

    bootstrap classes added

--- js/quickpager.jquery.js
+++ js/quickpager.jquery.js
@@ -2,7 +2,7 @@
     $.fn.quickPagination = function (options,fn) {
         var defaults = {
             pageSize: 10,
-            currentPage: 1,
+            active: 1,
             holder: null,
             pagerLocation: "after"
         };
@@ -10,30 +10,30 @@
         return this.each(function () {
             var selector = $(this);
             var pageCounter = 1;
-            selector.wrap("<div class='simplePagerContainer'></div>");
-            selector.parents(".simplePagerContainer").find("ul.simplePagerNav").remove();
+            selector.wrap("<div class='pagerContainer'></div>");
+            selector.parents(".pagerContainer").find("ul.pagerNav").remove();
             selector.children().each(function (i) {
                 if (i < pageCounter * options.pageSize && i >= (pageCounter - 1) * options.pageSize) {
-                    $(this).addClass("simplePagerPage" + pageCounter);
+                    $(this).addClass("pagerPage" + pageCounter);
                 } else {
-                    $(this).addClass("simplePagerPage" + (pageCounter + 1));
+                    $(this).addClass("pagerPage" + (pageCounter + 1));
                     pageCounter++;
                 }
             });
             selector.children().hide();
-            selector.children(".simplePagerPage" + options.currentPage).show();
+            selector.children(".pagerPage" + options.active).show();
             if (pageCounter <= 1) {
                 return;
             }
-            var pageNav = "<div class='row' style='clear:both'><ul class='pagination'>";
+            var pageNav = "<ul class='pagerNav pagination row' style='clear:both'>";
             for (i = 1; i <= pageCounter; i++) {
-                if (i == options.currentPage) {
-                    pageNav += "<li class='currentPage simplePageNav" + i + "'><a rel='" + i + "' href='#'>" + i + "</a></li>";
+                if (i == options.active) {
+                    pageNav += "<li class='active pageNav" + i + "'><a rel='" + i + "' href='#'>" + i + "</a></li>";
                 } else {
-                    pageNav += "<li class='simplePageNav" + i + "'><a rel='" + i + "' href='#'>" + i + "</a></li>";
+                    pageNav += "<li class='pageNav" + i + "'><a rel='" + i + "' href='#'>" + i + "</a></li>";
                 }
             }
-            pageNav += "</ul></div>";
+            pageNav += "</ul>";
             if (!options.holder) {
                 switch (options.pagerLocation) {
                 case "before":
@@ -49,18 +49,18 @@
             } else {
                 $(options.holder).append(pageNav);
             }
-            selector.parent().find(".simplePagerNav a").click(function () {
+            selector.parent().find(".pagerNav a").click(function () {
                 var clickedLink = $(this).attr("rel");
-                options.currentPage = clickedLink;
+                options.active = clickedLink;
                 if (options.holder) {
-                    $(this).parent("li").parent("ul").parent(options.holder).find("li.currentPage").removeClass("currentPage");
-                    $(this).parent("li").parent("ul").parent(options.holder).find("a[rel='" + clickedLink + "']").parent("li").addClass("currentPage");
+                    $(this).parent("li").parent("ul").parent(options.holder).find("li.active").removeClass("active");
+                    $(this).parent("li").parent("ul").parent(options.holder).find("a[rel='" + clickedLink + "']").parent("li").addClass("active");
                 } else {
-                    $(this).parent("li").parent("ul").parent(".simplePagerContainer").find("li.currentPage").removeClass("currentPage");
-                    $(this).parent("li").parent("ul").parent(".simplePagerContainer").find("a[rel='" + clickedLink + "']").parent("li").addClass("currentPage");
+                    $(this).parent("li").parent("ul").parent(".pagerContainer").find("li.active").removeClass("active");
+                    $(this).parent("li").parent("ul").parent(".pagerContainer").find("a[rel='" + clickedLink + "']").parent("li").addClass("active");
                 }
                 selector.children().hide();
-                selector.find(".simplePagerPage" + clickedLink).show();
+                selector.find(".pagerPage" + clickedLink).show();
                 fn && fn();
                 return false;
             });
